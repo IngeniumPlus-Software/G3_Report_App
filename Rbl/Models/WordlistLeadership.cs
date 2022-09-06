b222931978d5c4c4a8ac1184ef429c9b93cc922f
MRobb-IPS <mrobb@ingeniumplus.com>
2022-09-06 13:36:47 -0400

    updating context to have the new word list and df_all_ratios

---
+++ Rbl/Models/WordlistLeadership.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Rbl.Models
+{
+    public partial class WordlistLeadership
+    {
+        public string Column1 { get; set; }
+        public string Word { get; set; }
+        public int Total { get; set; }
+    }
+}
