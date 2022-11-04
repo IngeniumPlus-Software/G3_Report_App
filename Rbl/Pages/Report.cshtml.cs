1e950464b0856f7d0743f8489dca851d78539b62
MRobb-IPS <mrobb@ingeniumplus.com>
2022-11-04 15:17:19 -0400

    more updates + introductions to the first 4 actionable insights

--- Rbl/Pages/Report.cshtml.cs
+++ Rbl/Pages/Report.cshtml.cs
@@ -37,10 +37,10 @@ namespace Rbl.Pages
         public string HrEoReport5 { get; set; }
         public string Report6Narrative { get; set; }
         public string Report6FollowUp { get; set; }
-        public int G3HCAI_TalentPageNumber = 24;
-        public int G3HCAI_LeadershipPageNumber = 28;
-        public int G3HCAI_OrganizationPageNumber = 31;
-        public int G3HCAI_HrPageNumber = 36;
+        public int G3HCAI_TalentPageNumber = 26;
+        public int G3HCAI_LeadershipPageNumber = 32;
+        public int G3HCAI_OrganizationPageNumber = 37;
+        public int G3HCAI_HrPageNumber = 44;
 
 
         public async Task<IActionResult> OnGetAsync(string ticker)
